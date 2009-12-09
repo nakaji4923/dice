ef7b363bcebe39668f707f3a187c247a20c886d9
nakajima hideki <nakaji4923@gmail.com>
2009-12-09 11:11:56 +0900

    dice application

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,9 +1,12 @@
 class DiceController < ApplicationController
   def index
+		@rolls = Roll.find(:all, :order => "created_at DESC, id DESC", :limit =>5)
   end
 
   def shake
 		dice = Dice.new
 		@result = dice.shake
+		Roll.create :value => @result
   end
+
 end
